2942ecbc901a9902d04dd374354de1277e689f69
Andres Inguanzo <33536101+ainguanzo@users.noreply.github.com>
2018-10-18 17:00:41 -0400

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -4,7 +4,7 @@ var config = require('./config'),
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+  app.listen(process.env.PORT, function() {
+    console.log('App listening on port', process.env.PORT);
   });
 };
