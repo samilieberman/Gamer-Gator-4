62ba194db7a0a04cb2f56b125ecf26cbe40dc978
jmmarotta <julianmmarotta@gmail.com>
2018-11-08 15:32:44 -0500

    Added Functioning Date Time Picker

--- client/js/controllers/eventController.js
+++ client/js/controllers/eventController.js
@@ -1,49 +1,72 @@
 angular.module('events').controller('EventsController', ['$scope', 'Events',
-  function($scope, Events) {
+  function ($scope, Events) {
+
     /* Get all the listings, then bind it to the scope */
-    Events.getAll().then(function(response) {
+    Events.getAll().then(function (response) {
       $scope.events = response.data;
-    }, function(error) {
+    }, function (error) {
       console.log('Unable to retrieve listings:', error);
     });
 
     $scope.detailedInfo = undefined;
- 
-	  /**TODO 
-	  *Save the article using the Events factory. If the object is successfully
-	  saved redirect back to the list page. Otherwise, display the error
-	 */
-    $scope.addEvent = function() {
-      //$scope.listings.push($scope.newListing);
-      Events.create($scope.newEvent).then(function(response){
-        Events.getAll().then(function(response){
-        // redirect back to the list page
-        console.log(response.data);
-        $scope.events = response.data;
-        //window.location.href = '/';
-      }, function(error){
-        console.log('Unable to create events:', error);
+
+
+    $scope.months=[];
+    for (var i = 1; i <= 12; ++i) {
+      if (i < 10)
+        $scope.months.push("0" + i.toString());
+      else
+        $scope.months.push(i.toString());
+    }
+
+    $scope.days = [];
+    for (var i = 1; i <= 31; ++i) {
+      if (i < 10)
+        $scope.days.push("0" + i.toString());
+      else
+        $scope.days.push(i.toString());
+    }
+
+    $scope.hours = [];
+    for (var i = 1; i <= 24; ++i) {
+      if (i < 10)
+        $scope.hours.push("0" + i.toString());
+      else
+        $scope.hours.push(i.toString());
+    }
+
+    $scope.addEvent = function () {
+      var startTimeConcat = $scope.startTime.year + "-" + $scope.startTime.month + "-" + $scope.startTime.day + "T" + $scope.startTime.hour + ":00:00Z";
+      var endTimeConcat = $scope.endTime.year + "-" + $scope.endTime.month + "-" + $scope.endTime.day + "T" + $scope.endTime.hour + ":00:00Z";
+      //catch exceptions?
+      $scope.newEvent.startTime = new Date(startTimeConcat);
+      $scope.newEvent.endTime = new Date(endTimeConcat);
+
+      Events.create($scope.newEvent).then(function (response) {
+        Events.getAll().then(function (response) {
+          // redirect back to the list page
+          console.log(response.data);
+          $scope.events = response.data;
+          //window.location.href = '/';
+        }, function (error) {
+          console.log('Unable to create events:', error);
+        });
       });
-    });
       $scope.events = ""; // clears the form after submitting
     };
-    
-	   /**TODO
-        Delete the article using the Events factory. If the removal is successful,
-		    navigate back to 'listing.list'. Otherwise, display the error. 
-       */
-    $scope.deleteEvent = function(index) {
-      Events.delete($scope.events[index]._id).then(function(response){ // _id is from Mlab
-      Events.getAll().then(function(response){  // navigate back to 'listing.list'
-      }, function(error){
-        console.log('Unable to delete events:', error);
-        console.log(response.data);
+
+    $scope.deleteEvent = function (index) {
+      Events.delete($scope.events[index]._id).then(function (response) { // _id is from Mlab
+        Events.getAll().then(function (response) {  // navigate back to 'listing.list'
+        }, function (error) {
+          console.log('Unable to delete events:', error);
+          console.log(response.data);
+        });
       });
-    });
-      $scope.events.splice(index,1);
+      $scope.events.splice(index, 1);
     };
 
-    $scope.showDetails = function(index) {
+    $scope.showDetails = function (index) {
       $scope.detailedInfo = $scope.events[index];
     };
   }
