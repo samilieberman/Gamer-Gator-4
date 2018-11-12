a88ddf73d25cdb20b80682a78b25b36137d38e96
samilieberman <samilieberman1@gmail.com>
2018-11-11 22:57:07 -0500

    fixed comma issue

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-var localStrategy = require('passport-local').Strategy;
+//var localStrategy = require('passport-local').Strategy;
 //This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
