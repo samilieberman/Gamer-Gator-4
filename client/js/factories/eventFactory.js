a15d122e2b70583d7896bc75dccd4d6efa1448ff
Andres Inguanzo <33536101+ainguanzo@users.noreply.github.com>
2018-12-05 13:02:12 -0500

    Changed to heroku routes

--- client/js/factories/eventFactory.js
+++ client/js/factories/eventFactory.js
@@ -2,13 +2,13 @@ angular.module('events', []).factory('Events', function($http) {
   var methods = {
 
     getAll: function() {
-      return $http.get('http://localhost:8080/api/events');
-      //return $http.get('//gamergator.herokuapp.com/api/events');
+      //return $http.get('http://localhost:8080/api/events');
+      return $http.get('//gamergator.herokuapp.com/api/events');
     },
 
       create: function(event) {
-      return $http.post('http://localhost:8080/api/events', event);
-      //return $http.post('//gamergator.herokuapp.com/api/events', event);
+      //return $http.post('http://localhost:8080/api/events', event);
+      return $http.post('//gamergator.herokuapp.com/api/events', event);
     }, 
 
     delete: function(id) {
@@ -16,13 +16,13 @@ angular.module('events', []).factory('Events', function($http) {
         return result of HTTP delete method
        */
       //console.log(id);
-      return $http.delete('http://localhost:8080/api/events/' + id);
-      //return $http.delete('//gamergator.herokuapp.com/api/events/' + id);
+      //return $http.delete('http://localhost:8080/api/events/' + id);
+      return $http.delete('//gamergator.herokuapp.com/api/events/' + id);
     },
 
     update: function(id, event) {
-      return $http.put('http://localhost:8080/api/events/' + id, event);
-      //return $http.put('//gamergator.herokuapp.com/api/events/' + id, event);
+      //return $http.put('http://localhost:8080/api/events/' + id, event);
+      return $http.put('//gamergator.herokuapp.com/api/events/' + id, event);
 
     }
   };
